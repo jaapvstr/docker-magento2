da01f239ab60ef1ccdea962f73742185456ef2ea
jaapvstr <noreply@example.com>
2020-04-25 00:44:02 +0200

    initial file push

---
+++ integrator/ops/phpinfo.php
@@ -0,0 +1,12 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * phpinfo() wrapper to allow displaying only when configured to do so.
+ *
+ * @package PhpMyAdmin
+ */
+/**
+ * Displays PHP information
+ */
+    phpinfo();
+	
